e1cf4862bba620ff70445afe75ab36b621042b71
Matthias Valvekens <matthias@mvalvekens.be>
2022-05-14 00:27:13 +0200

    Make CVItemList implement IBlockElement directly

--- src/main/java/be/mvalvekens/cv/components/CVItemList.java
+++ src/main/java/be/mvalvekens/cv/components/CVItemList.java
@@ -7,13 +7,18 @@ import com.itextpdf.kernel.pdf.tagutils.AccessibilityProperties;
 import com.itextpdf.layout.borders.Border;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Div;
+import com.itextpdf.layout.element.IBlockElement;
+import com.itextpdf.layout.element.IElement;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.properties.TextAlignment;
 import com.itextpdf.layout.properties.UnitValue;
 import com.itextpdf.layout.properties.VerticalAlignment;
+import com.itextpdf.layout.renderer.IRenderer;
 
-public class CVItemList {
+import java.util.List;
+
+public class CVItemList implements IBlockElement {
     private final Table backingTable;
     private final boolean zeropad;
     private final ICVContext context;
@@ -60,8 +65,59 @@ public class CVItemList {
         this.backingTable.addCell(c);
     }
 
-    public void addToContainer(CVSection container) {
-        container.add(backingTable);
+    @Override
+    public List<IElement> getChildren() {
+        return backingTable.getChildren();
+    }
+
+    @Override
+    public void setNextRenderer(IRenderer renderer) {
+        backingTable.setNextRenderer(renderer);
+    }
+
+    @Override
+    public IRenderer getRenderer() {
+        return backingTable.getRenderer();
+    }
+
+    @Override
+    public IRenderer createRendererSubTree() {
+        return backingTable.createRendererSubTree();
+    }
+
+    @Override
+    public boolean hasProperty(int property) {
+        return backingTable.hasProperty(property);
+    }
+
+    @Override
+    public boolean hasOwnProperty(int property) {
+        return backingTable.hasOwnProperty(property);
+    }
+
+    @Override
+    public <T1> T1 getProperty(int property) {
+        return backingTable.getProperty(property);
+    }
+
+    @Override
+    public <T1> T1 getOwnProperty(int property) {
+        return backingTable.getOwnProperty(property);
+    }
+
+    @Override
+    public <T1> T1 getDefaultProperty(int property) {
+        return backingTable.getDefaultProperty(property);
+    }
+
+    @Override
+    public void setProperty(int property, Object value) {
+        backingTable.setProperty(property, value);
+    }
+
+    @Override
+    public void deleteOwnProperty(int property) {
+        backingTable.deleteOwnProperty(property);
     }
 
 }
